e4884ac93523a1ad15582fc239782b5b0c2c4ac3
Lerny <thelerny@gmail.com>
2024-11-26 18:55:46 +0500

    Update theme

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     echo -n (set_color normal)(basename (prompt_pwd))
-    echo -n (set_color green)(fish_git_prompt)
+    echo -n (set_color red)(fish_git_prompt)
     echo -n ' '
     echo -n (set_color yellow)'λ '
     set_color normal
