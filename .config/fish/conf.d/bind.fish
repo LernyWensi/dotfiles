0dee1201a2e674f4a10ef12d4f98b64ac4d62e6e
Lerny <thelerny@gmail.com>
2025-04-24 12:59:41 +0500

    Update fish keybindings to use new format

--- .config/fish/conf.d/bind.fish
+++ .config/fish/conf.d/bind.fish
@@ -1,7 +1,4 @@
-bind --user \cf 'fg 2>/dev/null; commandline -f repaint'
+bind --user ctrl-z 'fg 2>/dev/null; commandline -f repaint'
 
-bind --user \cz undo
-bind --user \cy redo
-
-bind --user \e\[1\;5D prevd-or-backward-word
-bind --user \e\[1\;5C nextd-or-forward-word
+bind --user ctrl-left prevd-or-backward-word
+bind --user ctrl-right nextd-or-forward-word
