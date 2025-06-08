6a7f279e7802b24f3c2776788b459c4b1de014d7
Lerny <thelerny@gmail.com>
2025-06-08 19:29:06 +0500

    Add rustup completions for fish and update keybindings

--- .config/fish/conf.d/bind.fish
+++ .config/fish/conf.d/bind.fish
@@ -1,4 +1,12 @@
-bind --user ctrl-z 'fg 2>/dev/null; commandline -f repaint'
+bind --user ctrl-f 'fg 2>/dev/null; commandline -f repaint'
+
+bind --user backspace backward-delete-char
+bind --user ctrl-h backward-kill-word
+bind --user delete delete-char
+bind --user ctrl-delete kill-word
+
+bind --user ctrl-z undo
+bind --user ctrl-y redo
 
 bind --user ctrl-left prevd-or-backward-word
 bind --user ctrl-right nextd-or-forward-word
